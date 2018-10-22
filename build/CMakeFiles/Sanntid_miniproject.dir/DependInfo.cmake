1d0ef62b3163223b5c26022f1696df95feb581b9
nodix95 <dino.mehmedagic95@gmail.com>
2018-10-22 14:01:29 +0200

    Updeted cmake

---
+++ build/CMakeFiles/Sanntid_miniproject.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/student/Desktop/TTK4147_miniproject/miniproject-files/network/udp.c" "/home/student/Desktop/TTK4147_miniproject/build/CMakeFiles/Sanntid_miniproject.dir/home/student/Desktop/TTK4147_miniproject/miniproject-files/network/udp.c.o"
+  "/home/student/Desktop/TTK4147_miniproject/src/main.c" "/home/student/Desktop/TTK4147_miniproject/build/CMakeFiles/Sanntid_miniproject.dir/home/student/Desktop/TTK4147_miniproject/src/main.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "PROJECT_SOURCE_DIR=\"/home/student/Desktop/TTK4147_miniproject/build\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
