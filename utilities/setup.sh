96d153e0ae25d4c1a05757fd167a0fcb356059a0
Jørn Bøni Hofstad <jornbh@stud.ntnu.no>
2018-10-29 10:24:36 +0100

    Was not able to do everything I wanted with te setup file

--- utilities/setup.sh
+++ utilities/setup.sh
@@ -1,10 +1,16 @@
 
+echo "git pull"
 git pull
-Inet_Address=$( ifconfig | grep  addr | python2 getInet.py)
+echo ""
 
+Inet_Address=$( ifconfig | grep  addr | python2 getInet.py)
 # Inet_Address=${GET_Inet_Address}
-echo ${Inet_Address}
+echo "IP address = ${Inet_Address} (Already changed in ../src/main.c)"
+echo ""
 echo ${Inet_Address} | python2 insertIP.py
 
-gnome-terminal -x "minicom -D /dev/ttyS0"
+echo press CTR A and Z 
+echo Press ctrl o and set the serial prot to 115200 
+echo "sudo minicom -D /dev/ttyS0"
+
 
