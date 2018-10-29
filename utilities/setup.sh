e10411e5477fe934f4e4a0df10be16bdb94235d7
Jørn Bøni Hofstad <jornbh@stud.ntnu.no>
2018-10-29 09:58:55 +0100

    Not tested yet

--- utilities/setup.sh
+++ utilities/setup.sh
@@ -1,9 +1,10 @@
-SPAWN="gnome-terminal -x"
 
-
+git pull
 Inet_Address=$( ifconfig | grep  addr | python2 getInet.py)
 
 # Inet_Address=${GET_Inet_Address}
 echo ${Inet_Address}
 echo ${Inet_Address} | python2 insertIP.py
+
+gnome-terminal -x "minicom -D /dev/ttyS0"
 
