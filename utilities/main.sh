2a9f700a00a8b0f68d0afc13dd77f1c1eb801de7
Jørn Bøni Hofstad <jornbh@stud.ntnu.no>
2018-11-05 13:41:21 +0100

    Dependencies

--- utilities/main.sh
+++ utilities/main.sh
@@ -12,17 +12,17 @@ echo "Starting server"
 ./miniproject-server
 
 
-rm plot5.png
-gnuplot plot5
-feh plot5.png
-cd ../utilities
-rm -f BOARD_DATA.txt
-mv /export/nfs/BOARD_DATA.txt BOARD_DATA.txt
-echo "COPIED"
-#cat BOARD_DATA.txt | python translate_to_plotable.py > results.dat
-cat BOARD_DATA.txt | python translate_to_plotable.py > results.dat
-echo "TRANSLATED"
-gnuplot testGnuplot
-feh plot5.png
+rm plot5.png && \
+gnuplot plot5 && \
+feh plot5.png && \
+cd ../utilities && \
+rm -f BOARD_DATA.txt && \
+mv /export/nfs/BOARD_DATA.txt BOARD_DATA.txt && \
+echo "COPIED" && \
+#cat BOARD_DATA.txt | python translate_to_plotable.py > results.dat && \
+cat BOARD_DATA.txt | python translate_to_plotable.py > results.dat && \
+echo "TRANSLATED" && \
+gnuplot testGnuplot && \
+feh plot5.png && \
 echo "Finished main.sh!"
 
